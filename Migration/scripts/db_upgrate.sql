ee4808c2b6910ef8474b356478b1467f52592e20
Ivan Muller <mullerivan@gmail.com>
2014-03-15 14:20:51 +1300

    borrando fotos  malas

--- Migration/scripts/db_upgrate.sql
+++ Migration/scripts/db_upgrate.sql
@@ -4,6 +4,15 @@ delete from SiteTree where ParentID = 15;
 select id from SiteTree where parentId not in (select id from SiteTree) and parentId <> 0 ;
 delete from SiteTree where id in (452,453,16, 58, 69, 70 , 71 , 72)
 
+CREATE TABLE File_tmp LIKE File; INSERT File_tmp SELECT * FROM File;
+
+delete from File where id in (SELECT GROUP_CONCAT(tmp.ID) FROM File_tmp as tmp GROUP BY tmp.Filename HAVING (COUNT( tmp.Filename ) >1)) or ID in (SELECT GROUP_CONCAT(tmp.ID) FROM File_tmp as tmp GROUP BY tmp.Title HAVING (COUNT( tmp.Title ) >1)) or ID in (SELECT GROUP_CONCAT(tmp.ID) FROM File_tmp as tmp GROUP BY tmp.Name HAVING (COUNT( tmp.Name ) >1)) or ParentId =5984 or ID =5984;
+Query OK, 157 rows affected (44.23 sec)
+
+DROP TABLE File_tmp;
+
+
+
 
 ,);
  */
